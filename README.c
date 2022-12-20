9db8a722c1276cce9e20cff62a51d5c346d1912e
01011888332 <111289703+01011888332@users.noreply.github.com>
2022-12-20 13:42:13 +0200

    Update README.c

--- README.c
+++ README.c
@@ -1,42 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-//fscanf(file,%d %d,&Height,&Width);         //مش عارف هيبقي كده ولا لا
-const int Width=7,Height=5;
+int Width=7,Height=5;
 char place[Width][Height];
+char disc='X';
+int col=0;
 void start(){
     for(int i=0;i<Width;i++){
-        for(int j=0;j<Height;j++){
+        for(int j=0;j<=Height;j++){
             place[i][j]=' ';
         }
     }
 }
 
+void fillCol(){
+    for(int i=Height-1;i>=0;i--){
+        if(place[i][col]==' '){
+            place[i][col]=disc;
+            break;
+        }
+    }
+}
+
 void enterance(){
+    int mode;
     printf("choose your mode:\n\n");
     printf("          1- single player\n");
     printf("          2- multiplayer\n");
 }
+int modeNumber(int x){
+    scanf("%d",&x);
+    if(x==1){
 
-void boardd(int Width,int Height ){
+    }
+    else if(x==2){
+
+    }
+    else{
+        printf("Enter the right number");
+    }
+}
+
+void boardd(int Width,int Height){
     int i,j;
-    for(i =0;i<Width;i++){
-        for(j=0;j<Height;j++){
+    for(i =0;i<Height;i++){
+        for(j=0;j<Width;j++){
             printf("|---|");
         }
         printf("\n");
-        for(int x=0;x<Height;x++){
+        for(int x=0;x<Width;x++){
             printf("| %c |",place[i][j]);
         }
         printf("\n");
     }
     for(int y=0;y<Height;y++){
-        printf("-----");
+        printf("-------");
     }
 }
 
 int main(){
-    enterance();
     boardd(Width,Height);
     return 0;
 }
