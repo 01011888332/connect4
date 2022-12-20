1a6231c584fafd6beb5ab4a87386ac40aaea92be
01011888332 <111289703+01011888332@users.noreply.github.com>
2022-12-20 11:35:44 +0200

    Update README.c

--- README.c
+++ README.c
@@ -1,2 +1,20 @@
 #include <studio.h>
-#5555555555
+#include <stdlib.h>
+#include <string.h>
+
+void boardd(int Height,int Width){
+    //fscanf(file,%d %d,&Height,&Width);         //مش عارف هيبقي كده ولا لا
+    for(int i =0;i<Width;i++){
+        for(int j=0;j<Height;j++){
+            printf("|---|");
+        }
+        printf("\n");
+        for(int x=0;x<Height;x++){
+            printf("|   |");
+        }
+        printf("\n");
+    }
+    for(int y=0;y<Height;y++){
+        printf("-----");
+    }
+}
